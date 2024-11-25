31a8859785d5df14e19e3b9742c09a57924abcab
Mani Movassagh <mani.mgh@gmail.com>
2024-11-25 19:09:03 +0100

    add model

--- Noter/Data/ApplicationDbContext.cs
+++ Noter/Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Noter.Models;
 
-namespace Noter.Data;
-
-public class ApplicationDbContext : IdentityDbContext
+namespace Noter.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet to manage TodoItem entities in the database
+        public required DbSet<TodoItem> TodoItems { get; set; } 
     }
 }
