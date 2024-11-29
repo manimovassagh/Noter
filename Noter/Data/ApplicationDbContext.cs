b617ae66ec5593dee949ae551ce9e5bc2b5568b6
Mani Movassagh <mani.mgh@gmail.com>
2024-11-29 19:57:04 +0100

    db

--- Noter/Data/ApplicationDbContext.cs
+++ Noter/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using Noter.Models;
 
 namespace Noter.Data
 {
+    
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
         public required DbSet<TodoItem> TodoItems { get; set; } 
